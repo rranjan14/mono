c7e629de5818c8371e446cf87ed10bf892a223d7
Greg Baker <greg@roci.dev>
2025-09-16 17:15:58 -0700

    refactor(zbugs): back to csv for seeding (#4901)
    This will allow us to use the existing gigabugs dataset.

--- apps/zbugs/scripts/seed.ts
+++ apps/zbugs/scripts/seed.ts
@@ -1,45 +1,66 @@
 import {fileURLToPath} from 'url';
 import {dirname, join} from 'path';
 import * as fs from 'fs';
-import {db} from '../db/db.ts';
-import {sql} from 'drizzle-orm';
+import postgres from 'postgres';
+import {pipeline} from 'stream/promises';
+import '../../../packages/shared/src/dotenv.ts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const CSV_FILE_PATTERN = /^\d+_(?<tableName>.*)\.csv$/;
 
 async function seed() {
-  const dataDir = join(__dirname, '../db/seed-data/github/');
+  const dataDir =
+    process.env.ZERO_SEED_DATA_DIR ??
+    join(__dirname, '../db/seed-data/github/');
+
+  console.log(process.env.ZERO_UPSTREAM_DB);
+
+  const sql = postgres(process.env.ZERO_UPSTREAM_DB as string);
 
   try {
-    if (
-      (await (
-        await db.execute(sql.raw('select 1 from issue limit 1'))
-      ).rowCount) === 1
-    ) {
-      console.log('Database already seeded.');
-    } else {
-      const files = fs
-        .readdirSync(dataDir)
-        .filter(file => file.endsWith('.sql'))
-        // apply in sorted order
-        .sort();
+    const files = fs
+      .readdirSync(dataDir)
+      .filter(f => f.match(CSV_FILE_PATTERN))
+      // apply in sorted order
+      .sort();
 
-      if (files.length === 0) {
-        console.log('No *.sql files found to seed.');
-        process.exit(0);
-      }
-
-      // Use a single transaction for atomicity
-      await db.transaction(async tx => {
-        for (const file of files) {
-          const filePath = join(dataDir, file);
-          const sqlContent = fs.readFileSync(filePath, 'utf-8');
-          await tx.execute(sql.raw(sqlContent));
-        }
-      });
-
-      console.log('✅ Seeding complete.');
+    if (files.length === 0) {
+      console.log(`No ${CSV_FILE_PATTERN} files found to seed.`);
+      process.exit(0);
     }
+
+    // Use a single transaction for atomicity
+    await sql.begin(async sql => {
+      let checkedIfAlreadySeeded = false;
+      for (const file of files) {
+        const filePath = join(dataDir, file);
+
+        const match = CSV_FILE_PATTERN.exec(file);
+        if (!match?.groups) {
+          continue;
+        }
+        const tableName = match.groups.tableName;
+
+        if (!checkedIfAlreadySeeded) {
+          const result = await sql`select 1 from ${sql(tableName)} limit 1`;
+          if (result.length === 1) {
+            console.log('Database already seeded.');
+            return;
+          }
+          checkedIfAlreadySeeded = true;
+        }
+        console.log(`Seeding table ${tableName} with rows from ${filePath}.`);
+        const fileStream = fs.createReadStream(filePath, {
+          encoding: 'utf8',
+        });
+        const query =
+          await sql`COPY ${sql(tableName)} FROM STDIN DELIMITER ',' CSV HEADER`.writable();
+        await pipeline(fileStream, query);
+      }
+    });
+
+    console.log('✅ Seeding complete.');
     process.exit(0);
   } catch (err) {
     console.error('❌ Seeding failed:', err);
