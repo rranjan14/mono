d6218c6959b1b20a4a03ca88d01d6e6e31dfa9c6
Matt Wonlaw <matt.wonlaw@gmail.com>
2025-09-22 13:31:07 -0400

    chore(zero): enable all chinook tests again (#4924)
    accidentally left this disabled after debugging

--- packages/zql-integration-tests/src/chinook/chinook.pg-test.ts
+++ packages/zql-integration-tests/src/chinook/chinook.pg-test.ts
@@ -32,7 +32,6 @@ describe(
         {
           suiteName: 'compiler_chinook',
           pgContent,
-          only: 'compare primary key',
           zqlSchema: schema,
           setRawData: r => {
             data = r;
